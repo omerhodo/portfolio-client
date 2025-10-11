878965d81437b88e05d53fc13c6f4df3f43a9ca5
Ömer Hodo <omerhodo@gmail.com>
2025-10-11 12:47:07 +0300

    add loading comp

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,16 @@
+const Loading = () => {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div>Yükleniyor...</div>
+    </div>
+  );
+};
+
+export default Loading;
